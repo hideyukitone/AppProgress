7e5244aec64b5b07f2275347d626a3d40514631a
hideyuki <hideyukitone@mbp-retina.local>
2016-11-04 17:00:52 +0900

    Initial Commit

---
+++ AppProgress/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppProgress
+//
+//  Created by 大國 嗣元 on 2016/11/04.
+//  Copyright © 2016年 hideyuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
